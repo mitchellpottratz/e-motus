6b65ff58771b20b17db378771a7d8950829fcf41
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-11-24 18:29:43 -0600

    index route can get all of the comments for a post

--- resources/comments.py
+++ resources/comments.py
@@ -10,6 +10,37 @@ from peewee import DoesNotExist
 comments = Blueprint('comments', 'comments')
 
 
+# index route - gets all of the comments for a post
+@comments.route('/', methods=['GET'])
+@login_required
+def get_post_comment():
+
+	# gets json data from client
+	data = request.get_json()
+
+	try:
+		# gets post by its id
+		post = Post.get(Post.id == data['post_id'], Post.soft_delete == False)
+	
+		# iterate through all of the posts comments and convert each post
+		# to a dictionary
+		comment_list = []
+		for comment in post.comments:
+			comment_list.append(model_to_dict(comment))
+
+		return jsonify(
+			data=comment_list,
+			status={'code': 201, 'message': 'successfully created comment.'}
+		)
+
+	# if the queried post doesnt exist
+	except DoesNotExist:
+		return jsonify(
+			data={},
+			status={'code': 404, 'message': 'Resource does not exist.'}
+		)
+
+
 # create route - creates a new comment
 @comments.route('/', methods=['POST'])
 @login_required
