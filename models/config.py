bc87038198876af89def5e5a9f57d3f2ea44b5e8
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-12-06 16:07:41 -0600

    added db connection before model imports

--- models/config.py
+++ models/config.py
@@ -2,18 +2,17 @@ from playhouse.db_url import connect
 import os
 from peewee import *
 
+if 'ON_HEROKU' in os.environ: 
+  DATABASE = connect(os.environ.get('DATABASE_URL')) 
+else:
+  DATABASE = SqliteDatabase('emotus.sqlite')
+
 # models from other files
 from .user import User
 from .post import Post
 from .like import Like
 from .follow import Follow
 from .comment import Comment
-
-
-if 'ON_HEROKU' in os.environ: 
-  DATABASE = connect(os.environ.get('DATABASE_URL')) 
-else:
-  DATABASE = SqliteDatabase('emotus.sqlite')
 
 
 # this method is called when the server starts
