a7fbd1dfffbab0e776a1777766d317d3727f150d
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-12-06 15:06:24 -0600

    added headers to setting to after_request function

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ def before_request():
 # called after every request
 @app.after_request
 def after_request(response):
+	response.headers.add('Access-Control-Allow-Origin', '*')
 	g.db.close() # closes database connection
 	return response # returns response to client
 
--- models/config.py
+++ models/config.py
@@ -2,6 +2,13 @@ from playhouse.db_url import connect
 import os
 from peewee import *
 
+# models from other files
+from .user import User
+from .post import Post
+from .like import Like
+from .follow import Follow
+from .comment import Comment
+
 
 if 'ON_HEROKU' in os.environ: # later we will manually add this env var 
                               # in heroku so we can write this code
@@ -10,15 +17,7 @@ if 'ON_HEROKU' in os.environ: # later we will manually add this env var
                                                      # when you provision the
                                                      # Heroku Postgres Add-on
 else:
-  DATABASE = SqliteDatabase('emotus', user='mitchellpottratz')
-
-
-# models from other files
-from .user import User
-from .post import Post
-from .like import Like
-from .follow import Follow
-from .comment import Comment
+  DATABASE = SqliteDatabase('emotus.sqlite')
 
 
 # this method is called when the server starts
